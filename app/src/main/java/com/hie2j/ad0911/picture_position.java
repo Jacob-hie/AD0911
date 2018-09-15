0dc798c8c69d2e0cb6ae53577e1ef162c7155126
Jacob <634865739@qq.com>
2018-09-15 13:58:43 +0800

    Initial commit

---
+++ app/src/main/java/com/hie2j/ad0911/picture_position.java
@@ -0,0 +1,13 @@
+package com.hie2j.ad0911;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class picture_position extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_position);
+    }
+}
